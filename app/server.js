71b1f005599d44c9a128d709cb26ebaa816c0f7a
Kanisuru <kanisurudaniel@gmail.com>
2026-07-22 12:43:36 +0100

    Initial Kansure secure infra + app

---
+++ app/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const version = process.env.APP_VERSION || 'v1';
+
+app.get('/', (req, res) => {
+    res.send('Hello from Kansure Web App - ${version}');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
